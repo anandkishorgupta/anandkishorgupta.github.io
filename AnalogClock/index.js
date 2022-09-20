ba390e6b9427df2fde59552bdc15f08dcf78f76a
anandkishorgupta <anandgupta20588@gmail.com>
2022-09-20 20:17:12 +0545

    final commit

---
+++ AnalogClock/index.js
@@ -0,0 +1,19 @@
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
+
+
+
+setInterval(() => {
+    d = new Date(); //object of date()
+    console.log(d);
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
